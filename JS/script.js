d4e4a49630bf40bdd22619005453b76599dc4ef7
rahulvarma07 <rahulvarma070707@gmail.com>
2025-07-11 14:14:39 +0530

    need to integrate js and complete footer page

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+const video = document.getElementById("foodVideo");
+const playButton = document.getElementById("playButton");
+
+playButton.addEventListener("click", () => {
+  if (video.paused) {
+    video.play();
+    playButton.style.display = "none";
+  } else {
+    video.pause();
+    playButton.style.display = "flex";
+  }
+});
+
+video.addEventListener("click", () => {
+  if (!video.paused) {
+    video.pause();
+    playButton.style.display = "flex";
+  }
+});
